da5aedc3f534ef6d620287af8f46ecf59c366b3d
Anish Agrawal <103357921+Anish29801@users.noreply.github.com>
2024-01-04 12:26:34 +0530

    Test

--- tagCreater.js
+++ tagCreater.js
@@ -18,7 +18,7 @@ async function createTag(orderid) {
     parameter: [
       {
         key: "trackingId",
-        type: "template",
+        type: "PageView",
         value: "UA-1234-5",
       }
     ]
